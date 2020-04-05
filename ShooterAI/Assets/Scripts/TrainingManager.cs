a968bf85be287320c29f5e95d20bc09a2be23323
Maganik <valentingrgrandjean@gmail.com>
2020-04-06 00:35:19 +0200

    Creation of all training terrain for a generation

--- ShooterAI/Assets/Scripts/TrainingManager.cs
+++ ShooterAI/Assets/Scripts/TrainingManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class TrainingManager : MonoBehaviour
 {
+    public GameObject prefab;
+    public int population;
+    public float simulationTime;
+    public List<int> architecture;
+    private Generation generation;
+    private List<GameObject> simulations;
     // Start is called before the first frame update
     void Start()
     {
-        
+        simulations = new List<GameObject>(population);
+        generation = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
+        for(int i = 0; i< population;i++)
+        {
+           simulations[i] =  GameObject.Instantiate(prefab, new Vector3(12 * i, 0), Quaternion.Euler(90,0,0));
+        }
     }
 
     // Update is called once per frame
