fd595ff4c6df31063e04b72eaf4add5b2ebf2b24
Arthur <afalzon12@orange.fr>
2020-04-05 18:21:16 +0200

    Generation.cs

---
+++ ShooterAI/Assets/Scripts/Generation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Generation
+{
+    private List<Genome> currentGen;
+    private List<float> fitness;
+    private int nbGenome;
+
+    public Generation(int population, int genomeSize)
+    {
+        fitness = new List<float>(population);
+
+        for(int i = 0; i < population; i++)
+        {
+            currentGen[i] = new Genome(genomeSize);
+            fitness[i] = 0.0f;
+        }
+    }
+
+    public List<float> Fitness
+    {
+        set { fitness = value; }
+    }
+
+    public void nextGen()
+    {
+
+    }
+}
