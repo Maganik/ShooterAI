84fcbf7e8202a20e3ae938ceb3ae6f10aa7536a2
Maganik <valentingrgrandjean@gmail.com>
2020-04-07 01:05:06 +0200

    v1 of training everything working

--- ShooterAI/Assets/Scripts/Generation.cs
+++ ShooterAI/Assets/Scripts/Generation.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 public class Generation
 {
@@ -28,9 +30,8 @@ public class Generation
     //Compute to next generation
     public void nextGen(float frequency, float magnitude)
     {
-        System.Random rand = new System.Random(); //Instantiate random number to be used
         List<Genome> nextGen = new List<Genome>();
-
+        
         //Sorting Variables
         float fitMax = 0.0f;
         float tempFloat = 0.0f;
@@ -40,23 +41,24 @@ public class Generation
         List<int> probaTab = new List<int>();
         int probaTemp1;
         int probaTemp2;
-
+        
         //NextGen Variables
         Genome tempGenome = new Genome(currentGen[0].Size);
         float frequencyOfMutation = frequency; //Probability of mutation
         float forceOfMutation = magnitude; //Magnitude of change when mutate
-
+        
         //Sorting
-        for(int i = 0; i < this.nbGenome; i++)
+        for (int i = 0; i < this.nbGenome; i++)
         {
-            for(int j = 0; j < this.nbGenome-1; i++)
+            for(int j = i; j < this.nbGenome-1; j++)
             {
-                if (fitness[j] > fitness[j + 1])
+                if (fitness[j] > fitMax)
                 {
                     fitMax = fitness[j];
                     tempIndex = j;
                 }
             }
+            
             //Sorting algorithme
             tempFloat = fitness[i];
             tempGenome = currentGen[i];
@@ -65,30 +67,31 @@ public class Generation
             fitness[tempIndex] = tempFloat;
             currentGen[tempIndex] = tempGenome;
         }
-
+        
         //Setting probabilities
-        for(int i = 0; i < this.nbGenome; i++)
+        for (int i = 0; i < this.nbGenome; i++)
         {
             for(int j = i; j < this.nbGenome; j++)
             {
                 probaTab.Add(i);
             }
         }
-
+        
         //Picking algorithme
-        for(int i = 0; i < nbGenome; i++)
+        for (int i = 0; i < nbGenome; i++)
         {
             //Proba settings
-            probaTemp1 = rand.Next(probaTab.Count);
-           /* do
-            {*/
-                probaTemp2 = rand.Next(probaTab.Count);
-           // } while (probaTab[probaTemp1] == probaTab[probaTemp2]);
+            probaTemp1 = Random.Range(0, probaTab.Count);
+           do
+            {
+                probaTemp2 = Random.Range(0, probaTab.Count);
+            } while (probaTab[probaTemp1] == probaTab[probaTemp2]);
 
             //Forming nextgen
-            nextGen[i] = currentGen[probaTab[probaTemp1]] + currentGen[probaTab[probaTemp2]];
+            nextGen.Add(currentGen[probaTab[probaTemp1]] + currentGen[probaTab[probaTemp2]]);
             nextGen[i].mutate(frequencyOfMutation, forceOfMutation);
         }
+        
     }
 
     public Genome this[int i]
--- ShooterAI/Assets/Scripts/TrainingManager.cs
+++ ShooterAI/Assets/Scripts/TrainingManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TrainingManager : MonoBehaviour
@@ -8,7 +9,10 @@ public class TrainingManager : MonoBehaviour
     public int population;
     public float simulationTime;
     public List<int> architecture;
-    private Generation generation;
+    private Generation generationAgent1;
+    private Generation generationAgent2;
+    private Generation generationAgent3;
+    private Generation generationAgent4;
     private List<GameObject> simulations;
     private float remainingTime;
     public int nbGeneration = 1;
@@ -17,11 +21,17 @@ public class TrainingManager : MonoBehaviour
     {
         remainingTime = simulationTime;
         simulations = new List<GameObject>();
-        generation = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
+        generationAgent1 = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
+        generationAgent2 = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
+        generationAgent3 = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
+        generationAgent4 = new Generation(population, RecurentNet.getNbWeightForArchi(architecture));
         for (int i = 0; i < population; i++)
         {
             simulations.Add(GameObject.Instantiate(prefab, new Vector3(12 * i, 0), Quaternion.Euler(90, 0, 0)));
-            simulations[i].GetComponent<MatchManager>().genomeAgent1 = generation[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent1 = generationAgent1[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent2 = generationAgent2[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent3 = generationAgent3[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent4 = generationAgent4[i];
             simulations[i].GetComponent<MatchManager>().architecture = architecture;
         }
     }
@@ -30,6 +40,7 @@ public class TrainingManager : MonoBehaviour
     void Update()
     {
         remainingTime -= Time.deltaTime;
+
         if (remainingTime < 0f)
         {
             nextSimulationRound();
@@ -40,20 +51,47 @@ public class TrainingManager : MonoBehaviour
     public void nextSimulationRound()
     {
 
-        List<float> fitness = new List<float>();
+        List<float> fitnessAgent1 = new List<float>();
+        List<float> fitnessAgent2 = new List<float>();
+        List<float> fitnessAgent3 = new List<float>();
+        List<float> fitnessAgent4 = new List<float>();
+
         foreach (GameObject gameObject in simulations)
         {
-            fitness.Add(gameObject.GetComponent<MatchManager>().getFitness());
+            List<float> fitness = gameObject.GetComponent<MatchManager>().getFitness();
+
+            fitnessAgent1.Add(fitness[0]);
+            fitnessAgent2.Add(fitness[1]);
+            fitnessAgent3.Add(fitness[2]);
+            fitnessAgent4.Add(fitness[3]);
+
             Destroy(gameObject);
         }
+        
         simulations = new List<GameObject>();
-        generation.Fitness = fitness;
-        generation.nextGen(0.05f, .5f);
+   
+        generationAgent1.Fitness = fitnessAgent1;
+        generationAgent2.Fitness = fitnessAgent2;
+        generationAgent3.Fitness = fitnessAgent3;
+        generationAgent4.Fitness = fitnessAgent4;
+
+
+        generationAgent1.nextGen(0.05f, 0.5f);
+        generationAgent2.nextGen(0.05f, 0.5f);
+        generationAgent3.nextGen(0.05f, 0.5f);
+        generationAgent4.nextGen(0.05f, 0.5f);
+
+
         for (int i = 0; i < population; i++)
         {
             simulations.Add(GameObject.Instantiate(prefab, new Vector3(12 * i, 0), Quaternion.Euler(90, 0, 0)));
-            simulations[i].GetComponent<MatchManager>().genomeAgent1 = generation[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent1 = generationAgent1[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent2 = generationAgent2[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent3 = generationAgent3[i];
+            simulations[i].GetComponent<MatchManager>().genomeAgent4 = generationAgent4[i];
+            simulations[i].GetComponent<MatchManager>().architecture = architecture;
         }
+
         nbGeneration++;
     }
 }
